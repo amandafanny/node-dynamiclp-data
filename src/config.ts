02b8a9c98c677984bca7e4529b33eae7b1f15677
Amanda <z4ghjdyx@foxmail.com>
2023-08-17 16:45:13 +0800

    feat: apikey

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const rpcUrl =
-  "https://eth-goerli.g.alchemy.com/v2/lABX6fw0SvEGW2kn99xWCvO_zpGv";
+  "https://eth-goerli.g.alchemy.com/v2/9Zg-lABX6fw0SvEGW2kn99xWCvO_zpGv";
 
 // export const rpc =
 //   "https://eth-goerli.g.alchemy.com/v2/wxt4mRmkJ7dSffMidEpaxcVf-hTAashX";
